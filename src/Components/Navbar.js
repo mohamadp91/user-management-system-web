596a486d38dd2e30bb53a83f50399dcf8198b304
mohamadp91 <69380537+mohamadp91@users.noreply.github.com>
2020-10-10 15:27:45 +0330

    Add a float action bottun for add user (#17)

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,6 +4,9 @@ import Tab from '@material-ui/core/Tab';
 import TabContext from '@material-ui/lab/TabContext';
 import TabList from '@material-ui/lab/TabList';
 import styled from 'styled-components';
+import TabPanel from "@material-ui/lab/TabPanel";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from '@material-ui/icons/Add';
 
 const AppBarStyled = styled(AppBar)`
         background : #222;
@@ -11,18 +14,24 @@ const AppBarStyled = styled(AppBar)`
 
 const TabContextStyled = styled(TabContext)`
         flex-grow: 1;
-`
+`;
+
+const FabStyled = styled(Fab)`
+    position: absolute;
+    right: 16px;
+    bottom: 16px;
+`;
 
 export default function NavBar() {
 
-    const [value, setValue] = React.useState('1');
+    const [tabIndex, setTabIndex] = React.useState(String(1));
 
     const handleChange = (event, newValue) => {
-        setValue(newValue);
+        setTabIndex(newValue);
     };
 
     return (
-        <TabContextStyled value={value}>
+        <TabContextStyled value={tabIndex}>
             <AppBarStyled position="static">
                 <TabList onChange={handleChange}>
                     <Tab label="Users" value="1"/>
@@ -30,6 +39,11 @@ export default function NavBar() {
                     <Tab label="Manufacturers" value="3"/>
                 </TabList>
             </AppBarStyled>
+            <TabPanel value="1">
+                <FabStyled color="primary">
+                    <AddIcon/>
+                </FabStyled>
+            </TabPanel>
         </TabContextStyled>
     );
 }
