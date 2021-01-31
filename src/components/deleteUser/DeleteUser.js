d310cc453813acb28211912e572e7b01a8d72d95
mohamadp91 <mohamad7291m@gmail.com>
2021-02-01 01:37:25 +0330

    Implementation delete-user

---
+++ src/components/deleteUser/DeleteUser.js
@@ -0,0 +1,52 @@
+import React from "react"
+import { Button, Container, FormControl, FormGroup } from "@material-ui/core"
+import styled from "styled-components"
+import { useDispatch } from "react-redux"
+
+import { DELETE_USER } from "../../state"
+
+const ContainerStyled = styled(Container)`
+	flex-grow: 1;
+	background: #f5f5f5;
+`
+
+const FormGroupStyled = styled(FormGroup)`
+	padding: 30px;
+`
+
+const FormControlButtonStyled = styled(FormControl)`
+	margin-top: 30px;
+	flex-direction: row;
+	justify-content: space-between;
+`
+
+export const DeleteUser = ({ cancel, userId }) => {
+	const CONFIRMATION_MESSAGE = "Are you sure you want to delete this user ?"
+	const dispatch = useDispatch()
+
+	const deleteUser = () => {
+		dispatch({ type: DELETE_USER, payload: userId })
+		cancel()
+	}
+	return (
+		<ContainerStyled>
+			<FormGroupStyled>
+				{CONFIRMATION_MESSAGE}
+				<FormControlButtonStyled>
+					<Button onClick={cancel} variant="contained" color="secondary">
+						Cancel
+					</Button>
+					<Button
+						onClick={() => deleteUser()}
+						variant="contained"
+						color="primary"
+					>
+						Ok
+					</Button>
+				</FormControlButtonStyled>
+			</FormGroupStyled>
+		</ContainerStyled>
+	)
+}
+
+export default DeleteUser
--- src/state/store.js
+++ src/state/store.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware } from "redux"
 
 export const ADD_USER = "ADD_USER"
+export const DELETE_USER = "DELETE_USER"
 
 export const store = createStore(
 	(state, { type, payload }) => {
@@ -8,6 +9,8 @@ export const store = createStore(
 			case ADD_USER:
 				if (!state) return (state = [payload])
 				return (state = [...state, payload])
+			case DELETE_USER:
+				return state.filter((user) => user.id !== payload)
 			default:
 				return state
 		}
